4233fe1385b5a3e7b695d72f0e4d5676a2c9af48
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-06-17 21:14:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 def double(p):
-  if len(p) == 1:
-    if len(p[0]) == 1:
+  if len(p) == 1 and type(p) == list:
+    if len(p[0]) == 1 and type(p[0]) == list:
       return p[0][0]
   return p
+
+def is_dead(p):
+  s = True
+  for i in p:
+    if type(i) == list:
+      return True
+  return False
+  
+def remove_double(p):
+  if type(i) != list:
+    return
+  p = double(p)
+  for i in range(len(p)):
+    remove_double(p[i])
