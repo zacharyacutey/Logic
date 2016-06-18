47cb753a2ee89a42aa0d7cad3ea6b378ad65b31e
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-06-17 21:59:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 def double(p):
-  if len(p) == 1 and type(p) == list:
-    if len(p[0]) == 1 and type(p[0]) == list:
-      return p[0][0]
-  return p
-
-def is_dead(p):
-  s = True
-  for i in p:
-    if type(i) == list:
-      return True
-  return False
-  
-def remove_double(p):
   if type(p) != list:
-    return
-  p = double(p)
-  for i in range(len(p)):
-    remove_double(p[i])
+    return p
+  else:
+    if type(p[0]) == list:
+      if type(p[0][0]) == list:
+        return double(p[0][0])
+#Changes a ~~p => p in one item
+
+def remove_double(p):
+  pass
+#remove_double should remove all [[p]]'s and replace them with p's
+"""
+TODO: FIX THIS remove_double
+I CAN'T GET IT TO WORK!!!!!
+SCREW YOU PASS BY VALUE!
