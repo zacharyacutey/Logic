90d7388a2a2657e5390319f2dd865d6096786658
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-07-18 11:17:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,55 @@ def remove_double(arg):
     if type(arg[i]) == list:
       arg[i] = remove_double(arg[i])
   return arg
+truths = []
+
+def define_truth(p_):
+  p = remove_double(p_)
+  if p in truths:
+    return False
+  else:
+    truths.append(p)
+    return True
+
+def contrapositive(p):
+  return remove_double([[p[1]],[p[0]]])
+
+def add_contrapositives():
+  for i in truths:
+    if not ( contrapositive(i) in truths ):
+      define_truth(contrapositive(i))
+      return True
+  return False
+
+def add_transitives():
+  for i in truths:
+    for j in truths:
+      if i != j and len(i) == 2 and len(j) == 2 and type(i) == list and type(j) == list:
+        if i[1] == j[0]:
+          if not ([i[0],j[1]] in truths):
+            define_truth([i[0],j[1]])
+            return None
+            
+  return False
+
+def add_detachments():
+  for i in truths:
+    for j in truths:
+      if (len(i) == 1 or type(i) == str) and len(j) == 2 and type(j) == list:
+        if i == j[0]:
+          if not (j[1] in truths):
+            define_truth(j[1])
+            return True
+  return False
+
+def deduce():
+  if add_contrapositives():
+    deduce()
+    return None
+  if add_transitives():
+    deduce()
+    return None
+  if add_detachments():
+    deduce()
+    return None
+  return None
